c50dec042e7208c5c323b86c890998d3fda5f828
html <newparas11422@gmail.com>
2022-09-16 21:38:37 +0530

    loop

--- vuetuts/src/components/Home.vue
+++ vuetuts/src/components/Home.vue
@@ -1,43 +1,44 @@
 <template>
     <ol>
-      <h3>If Else Conditions</h3>
-      <h1 v-if="!conditions">not vue</h1>
-      <!-- <h1 v-if="!conditions">Good Night</h1> -->
-      <h1 v-else="!conditions">vue</h1>
-        <!-- <button class="con" v-on:click="toggle">hello everyone 😍</button> -->
-        <!-- more easy way to toggle -->
-        <button class="con" v-on:click="conditions=!conditions">hello everyone 😍</button>
-       
+      <h3>Loop</h3>
+       <li v-for="val in arrayy" :key="val">{{val}}</li><br>
+       <li v-for="val2 in newUser" :key="val2">Name = {{val2.name}}, and Email = {{val2.email}}.</li>
     </ol>
 </template>
 <script>
     export default{ 
         name:'home',
-      data(){
-        return{
-          //  conditions:false
-           conditions:true
+        data(){
+          return{
+            arrayy:['array!','Newarray#','conditions','else',22],
+            newUser:[{
+              name: 'paras',
+              location: 'delhi'
+              ,email: 'paras.com'
+            },
+            {
+              name: 'rohan',
+              location: 'gurgaon'
+              ,email: 'rohan.com'
+            },
+            {
+              name: 'mohan',
+              location: 'punjab'
+              ,email: 'mohan.com'
+            },
+            {
+              name: 'anuj',
+              location: 'goa'
+              ,email: 'anuj.com'
+            },
+
+          ]
+          }
         }
-      },
-      methods:{
-        toggle(){
-          this.conditions = !this.conditions
-        }
-      }
     }
     </script>
     <style scoped>
         h3{
             color: rgb(82, 37, 243);
-        }
-        label{
-            margin: 0 .5rem;
-        }
-        .con{
-          color: white;
-          background-color: blue;
-          padding: .5rem 1rem;
-          border-radius: .3rem;
-          border: none;
         }
     </style>
