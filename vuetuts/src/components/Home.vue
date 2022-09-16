7a1595512147acdcb5b3d5c76fc775a463965214
html <newparas11422@gmail.com>
2022-09-16 08:07:16 +0530

    checkbox and radio button value

--- vuetuts/src/components/Home.vue
+++ vuetuts/src/components/Home.vue
@@ -1,10 +1,30 @@
 <template>
     <ol>
-      <h3>get input field value</h3>
-      <input v-model="email" type="text">
-      <br>
-      <input v-model="password" type="text"><br>
-      <button v-on:click="getData" type="button">get values</button>
+      <h3>get checkbox & radio button value</h3>
+      <label for="java">Java</label>
+      <input type="checkbox" value="java" id="java" v-model="tech">
+      <label for="php">Php</label>
+      <input type="checkbox" value="php" id="php" v-model="tech">
+      <label for="vue">Vue</label>
+      <input type="checkbox" value="vue" id="vue" v-model="tech">
+
+      <p>{{tech}}</p>
+<br>
+      <h2>For check box</h2>
+      <h4>What is your age:</h4>
+      <label for="age1">0-20</label>
+      <input type="radio" name="age" id="age1" value="0-20" v-model="age">
+
+      <label for="age2">20-40</label>
+      <input type="radio" name="age" id="age2" value="20-40" v-model="age">
+
+      <label for="age1">40-60</label>
+      <input type="radio" name="age" id="age3" value="40-60" v-model="age">
+
+      <label for="age4">60-80</label>
+      <input type="radio" name="age" id="age4" value="60-80"  v-model="age">
+<br>
+<p>{{age}}</p>
     </ol>
 </template>
 <script>
@@ -12,8 +32,8 @@
         name:'home',
       data(){
         return{
-            email:null,
-            password:null
+            tech:[],
+            age:null
         }
       },
       methods:{
@@ -26,5 +46,8 @@
     <style scoped>
         h3{
             color: rgb(82, 37, 243);
+        }
+        label{
+            margin: 0 .5rem;
         }
     </style>
