2c0ded7631e16ab38a118aac354e33799aa2aae2
html <newparas11422@gmail.com>
2022-09-16 06:03:38 +0530

    events

--- vuetuts/src/components/Home.vue
+++ vuetuts/src/components/Home.vue
@@ -2,25 +2,34 @@
 
 <template>
     <ol>
-        <!-- <li>Home {{'not available'.length}}</li> -->
+        <span v-on:mousemove="test('mouse move')" >Home</span>
         <li>email {{email}}</li>
         <li>type {{gen('asus')}} </li>
         <li>intro {{getData().name}}</li>
         <li>intro {{getData().email}}</li>
         <li>newMobile {{getData().newMobile}}</li>
+
+        <!-- event -->
+        <button v-on:click="test(`  clicked by me`)" >click event</button>
+        <button v-on:dblclick="test(`  double clicked by me`)" >double click event</button>
+        <button v-on:click="plus">count {{count}}</button>
     </ol>
 </template>
 <script>
-    export default{
+    export default{ 
         name:'home',
         data(){
             return{
                 email:'example@gmail.comm',
                 mobile: '10001',
-               
+               count: 10
             }
         },
         methods: {
+            plus(){
+                this.count = this.count + 1
+            }
+            ,
             gen(model){
                     return  model
                     
@@ -31,8 +40,17 @@
                         email:this.email,
                         newMobile:this.mobile
                     }
+                },
+                test(data){
+                    return alert(data)
                 }
         },
 
     }
     </script>
+    <style scoped>
+        span{
+            background-color: blue;
+            color: white;
+        }
+    </style>
