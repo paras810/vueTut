1a5f39b852184e7286f1b08183bd2bc3f0ab1401
html <newparas11422@gmail.com>
2022-09-17 21:44:10 +0530

    class binding

--- vuetuts/src/components/Home.vue
+++ vuetuts/src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
-      <h2>Html Binding</h2>
-      <h4>{{stringg}}</h4>
-      <div v-html="tag"></div>
-      <div v-html="tag2"></div>
+      <h2>class Binding</h2>
+      <!-- <div class="green">component</div> -->
+      <div :class="applyStyle">component</div>
+      <button @click="colorFull=!colorFull">toggle background-color</button>
       
 </template>
 
@@ -10,9 +10,17 @@
     export default{
    data(){
     return{
-    stringg:'new user entered',
-    tag:`<h6>this is a heading </h6>`,
-    tag2:`<li>List </li>`,
+      colorFull:false
+    }
+   },
+   computed:{
+    applyStyle(){
+      return{
+        green:this.colorFull,
+        error:!this.colorFull,
+        other:this.colorFull
+        
+      }
     }
    }
 
@@ -21,5 +29,14 @@
     <style scoped>
         h2{
             color: rgb(82, 37, 243);
+        }
+        .green{
+          color: greenyellow;
+        }
+        .error{
+          background-color: rgb(228, 131, 131) ;
+        }
+        .other{
+          font-size: 20px;
         }
     </style>
