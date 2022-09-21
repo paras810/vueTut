2b640c8018297c391e6ba447850be31dac0e387e
html <newparas11422@gmail.com>
2022-09-21 23:22:49 +0530

    beforeUpdate and updated

--- vuetuts/src/components/Home.vue
+++ vuetuts/src/components/Home.vue
@@ -1,22 +1,35 @@
 <template>
-<h2>BeforeMount and mount </h2>
+<h2>BeforeUpdate and update </h2>
+<h4 ref="names">{{name}}</h4>
+<button @click="name = name + 1" >Update value</button>
 </template>
 
 <script>
 export default {
     data() {
         return {
-         name:'paras'
+         name:0
         }
     },
-  beforeMount() {
-      alert('beforMount')
-      console.log('beforeMount', this.$el)
-  },
-  mounted() {
-      alert('Mounted')
-      console.log('Mounted', this.$el)
-  },
+ beforeUpdate() {
+// alert('value is changing')
+
+//vlaue through data
+// console.log('beforUpdate',this.name)
+
+//vlaue through UI
+console.log('beforUpdate',this.$refs['names'].textContent)
+},
+updated() {
+    // alert('value changed')
+    
+    //vlaue through data
+    // console.log('updated',this.name)
+
+    //vlaue through UI
+    console.log('updated',this.$refs['names'].textContent)
+
+ },
 }
 </script>
 
