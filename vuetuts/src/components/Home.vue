6c3afd30534d9cbddad13a2cecf2e929b4e60785
html <newparas11422@gmail.com>
2022-09-21 23:53:50 +0530

    beforeUnmount and unmounted

--- vuetuts/src/components/Home.vue
+++ vuetuts/src/components/Home.vue
@@ -1,35 +1,18 @@
 <template>
-<h2>BeforeUpdate and update </h2>
-<h4 ref="names">{{name}}</h4>
-<button @click="name = name + 1" >Update value</button>
+<h2>BeforeUnmount and unmount </h2>
+<button @click="show = !show">Toggle</button>
+<User v-if="show" />
 </template>
 
 <script>
+import User from "./User.vue";
 export default {
     data() {
         return {
-         name:0
-        }
+            show: true 
+        };
     },
- beforeUpdate() {
-// alert('value is changing')
-
-//vlaue through data
-// console.log('beforUpdate',this.name)
-
-//vlaue through UI
-console.log('beforUpdate',this.$refs['names'].textContent)
-},
-updated() {
-    // alert('value changed')
-    
-    //vlaue through data
-    // console.log('updated',this.name)
-
-    //vlaue through UI
-    console.log('updated',this.$refs['names'].textContent)
-
- },
+    components: { User }
 }
 </script>
 
