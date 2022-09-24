cc50b3f0d0cf614abd0cc491747dec3cbca1178a
html <newparas11422@gmail.com>
2022-09-24 19:04:48 +0530

    list with api

--- vuetuts/src/App.vue
+++ vuetuts/src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>Call API</h1>
-<p v-for="item, index in list" :key="index">{{item.id}} --- {{item.email}}---{{item.first_name}}</p>
+  <h1>User Data</h1>
+
+  <ul class="item" v-for="item,index in list" :key="index">
+    <img :src="item.avatar" alt="">
+<li>{{item.id}}</li>
+<li>{{item.email}}</li>
+<li>{{item.first_name}}</li>
+  </ul>
 
   <!-- <a href="https://reqres.in/api/users?page=1">api</a> -->
 </template>
@@ -23,10 +29,28 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
   h1{
     color: rgb(80, 242, 40);
     background-color: black;
+  }
+  .item{
+    display: flex; 
+    list-style: none;
+    align-items: center;
+    justify-content: center;
+    /* border: 4px solid red; */
+  }
+  .item img{
+    width: 40px;
+    border-radius: 100%;
+  }
+  .item li{
+    text-align: center;
+    width: 400px;
+    /* border: 1px solid; */
+    padding: 5px;
+    display: inline-block;
   }
 </style>
 
